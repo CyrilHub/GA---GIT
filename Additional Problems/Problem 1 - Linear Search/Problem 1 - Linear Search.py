9a526a9f670f2bfaa48ef718d295dfaafc7818d7
Cyril <cyril.p.blank@gmail.com>
2019-01-03 20:00:20 -0500

    Added return none

--- Additional Problems/Problem 1 - Linear Search/Problem 1 - Linear Search.py
+++ Additional Problems/Problem 1 - Linear Search/Problem 1 - Linear Search.py
@@ -1,14 +1,14 @@
-object_list = ["Maria","Dana","David","Lauren","David"]
-look_for = "David"
-
 def finder(object,target):
     index = 0
     for name in object_list:
         #? Is it possible to get the index based on item of name?
-
         if name == target:
-            print (name + "-->" + format(index))
+            return index
         index += 1
+    return None
 
+
+object_list = ["Maria","Dana","David","Lauren","David"]
+look_for = "Dan"
 
 finder(object_list,look_for)
