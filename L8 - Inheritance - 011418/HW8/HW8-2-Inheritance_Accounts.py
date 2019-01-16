b31016e409ded4de1effeae687ad9b3d81e72429
Cyril <cyril.p.blank@gmail.com>
2019-01-16 18:48:35 -0500

    Updated the positive input checker, need to add the super(). to refer to another function

--- L8 - Inheritance - 011418/HW8/HW8-2-Inheritance_Accounts.py
+++ L8 - Inheritance - 011418/HW8/HW8-2-Inheritance_Accounts.py
@@ -1,21 +1,23 @@
+#Bank accounts keep track of their current balance
 class BankAccount:
     def __init__(self, act_balance=0):
         self.act_balance = act_balance
 
-    def positive_input(self, deposite_amount=0,withdraw_amount=0):
-        if deposite_amount > 0:
-            return True
+    def positive_input_checker(self, deposite_amount=0, withdraw_amount=0):
+        if deposite_amount > 0 or withdraw_amount > 0:
+            pass
         else:
             return False
     
-
-            
-
+          
+    #deposit method returns the balance of the account after adding the deposited amount
     def deposit(self, deposit_amount):
+        if positive_input_checker(deposit_amount) == False:
+            return False
         self.act_balance += deposit_amount
         print ("Deposit - Basic account has:", self.act_balance)
         return self.act_balance
-
+    #withdraw method returns the amount of money that was successfully withdrawn
     def withdraw(self, withdraw_amount):
         self.act_balance -= withdraw_amount
         print ("Withdraw - Amount:", withdraw_amount)
@@ -23,8 +25,6 @@ class BankAccount:
         return withdraw_amount
     
     
-    
-
 
 basic_account = BankAccount()
 basic_account.deposit(600)
