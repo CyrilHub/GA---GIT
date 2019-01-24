4c81a9544d48ae0ffa52fd3820d6e635b23cf225
Cyril <cyril.p.blank@gmail.com>
2019-01-23 20:41:00 -0500

    lesson 10

---
+++ L10/Class Notes - 1.py
@@ -0,0 +1,6 @@
+my_file = open("hello.txt")
+my_file_new = open("New_File.txt", "w")
+x = my_file.read()
+my_file_new.write("xxxxxxxxxxxxxxxxxxxxx")
+# print(my_file_new.read())
+
--- L8 - Inheritance - 011418/HW8/HW8-2-Inheritance_Accounts.py
+++ L8 - Inheritance - 011418/HW8/HW8-2-Inheritance_Accounts.py
@@ -1,10 +1,9 @@
 # Bank accounts keep track of their current balance
 class BankAccount:
-    interest_rate = 0.02
-    act_balance = 0
 
     def __init__(self, act_balance=0):
         self.act_balance = act_balance
+        self.interest_rate = 0.2
 
     
     # Return False if someone tries to deposit or withdraw a negative amount.
@@ -15,28 +14,44 @@ class BankAccount:
             return False
    
     #  Increase Acccount Balance by 2%
-    def accumulate_interest(self, deposite_amount=0, withdraw_amount=0):
-        if deposite_amount > 0:
-            self.act_balance += self.act_balance * interest_rate 
+    def accumulate_interest(self):
+        self.act_balance += act_balance * interest_rate    
+        return self.act_balance
           
     # Deposit method returns the balance of the account after adding the deposited amount
     def deposit(self, deposit_amount):
+        # Checks if input is positive
         if self.positive_input_checker(deposit_amount) == False:
             return False
+        # Adds deposite to account balance
         self.act_balance += deposit_amount
-        act_balance = self.act_balance 
-        self.accumulate_interest(self, deposite_amount, withdraw_amount)
-        print ("Deposit - Basic account has:", self.act_balance)
-        return self.act_balance
+        # Update act_balance at class level
+        # ??? why can't i assign it to act_balance direct
+        act_balance_class = self.act_balance
+        # Adds interestes rate
+        self.accumulate_interest(act_balance_class)
+        print ("Deposit - Amount:", deposit_amount)
+        print ("Deposit - Basic account has:", act_balance_class)
+        return act_balance_class
    
     # Withdraw method returns the amount of money that was successfully withdrawn
     def withdraw(self, withdraw_amount):
+        # Checks if input is positive
+        if self.positive_input_checker(withdraw_amount) == False:
+             return False
+        # Substract deposite to account balance
         self.act_balance -= withdraw_amount
+        # Update act_balance at class level
+        act_balance_class = self.act_balance
+         # Adds interestes rate
+        self.accumulate_interest(deposit_amount)
+        # ================================================  #
         print ("Withdraw - Amount:", withdraw_amount)
-        print ("Withdraw - Basic account has:", self.act_balance)
+        self.accumulate_interest(deposit_amount, withdraw_amount)
+        print ("Withdraw - Basic account has:", act_balance_class)
         return withdraw_amount
     
-    
+ 
 
 basic_account = BankAccount(100)
 basic_account.deposit(600)
