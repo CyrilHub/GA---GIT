36d52fd16a87fa80d36843bd4b8c00d4a04b5fe5
cblank <cblank@908devices.com>
2019-02-04 16:57:08 -0500

    HW 8 completed and starte Hw9

--- HW/Hw 8/problem2.py
+++ HW/Hw 8/problem2.py
@@ -7,43 +7,50 @@ employees = [
     "last_name": "Lumbergh",
     "job_title": "Vice President",
     "hire_date": 1985,
-    "performance_review": "excellent"
+    "performance_review": "excellent",
+    "finished_review": "yes"
   }, {
     "first_name": "Michael", 
     "last_name": "Bolton",
     "job_title": "Programmer",
     "hire_date": 1995,
-    "performance_review": "poor"
+    "performance_review": "poor",
+    "finished_review": "yes"
   }, {
     "first_name": "Peter", 
     "last_name": "Gibbons",
     "job_title": "Programmer",
     "hire_date": 1989,
-    "performance_review": "poor"
+    "performance_review": "poor",
+    "finished_review": "yes"
   }, {
     "first_name": "Samir", 
     "last_name": "Nagheenanajar",
     "job_title": "Programmer",
     "hire_date": 1974,
-    "performance_review": "fair"
+    "performance_review": "fair",
+    "finished_review": "yes"
   }, {
     "first_name": "Milton", 
     "last_name": "Waddams",
     "job_title": "Collator",
     "hire_date": 1974,
-    "performance_review": "does he even work here?"
+    "performance_review": "does he even work here?",
+    "finished_review": "yes"
   }, {
     "first_name": "Bob", 
     "last_name": "Porter",
     "job_title": "Consultant",
     "hire_date": 1999,
-    "performance_review": "excellent"
+    "performance_review": "excellent",
+    "finished_review": "yes"
   }, {
     "first_name": "Bob", 
     "last_name": "Slydell",
     "job_title": "Consultant",
     "hire_date": 1999,
-    "performance_review": "excellent"
+    "performance_review": "excellent",
+    "finished_review": "yes"
   }
 ]
 # This line opens or creates a `names.csv` file. 
@@ -53,7 +60,7 @@ employees = [
 with open('tps_report.csv', 'w', newline='') as csvfile:
 
   # These are the header row values at the top.
-  fieldnames = ['first_name', 'last_name','job_title', 'hire_date','performance_review' ]
+  fieldnames = ['first_name', 'last_name','job_title', 'hire_date','performance_review', 'finished_review']
   # This opens the `DictWriter`. 
   writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
   # Write out the header row (this only needs to be done once!). 
@@ -62,18 +69,18 @@ with open('tps_report.csv', 'w', newline='') as csvfile:
   for i in employees:
     writer.writerow(i)
 
-with open('tps_report.csv','r') as csvinput:
-    with open('tps_report.csv', 'w') as csvoutput:
-        writer = csv.writer(csvoutput, lineterminator='\n')
-        reader = csv.reader(csvinput)
+# with open('tps_report.csv','r') as csvinput:
+#     with open('tps_report.csv', 'w') as csvoutput:
+#         writer = csv.writer(csvoutput, lineterminator='\n')
+#         reader = csv.reader(csvinput)
 
-        all = []
-        row = next(reader)
-        row.append('finished_review')
-        all.append(row)
+#         all = []
+#         row = next(reader)
+#         row.append('finished_review')
+#         all.append(row)
 
-        for row in reader:
-            row.append(row[0])
-            all.append(row)
+#         for row in reader:
+#             row.append(row[0])
+#             all.append(row)
 
-        writer.writerows(all)
+#         writer.writerows(all)
