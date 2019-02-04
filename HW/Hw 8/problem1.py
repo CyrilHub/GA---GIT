04be1fd3e358f06f32681432aa2456dc54765d23
Cyril <cyril.p.blank@gmail.com>
2019-02-04 14:06:16 -0500

    home work

--- HW/Hw 8/problem1.py
+++ HW/Hw 8/problem1.py
@@ -46,38 +46,22 @@ employees = [
     "performance_review": "excellent"
   }
 ]
-# This line opens or creates a `names.csv` file. 
+ 
+print(employees[0])
 
-
+ 
 with open('tps_report.csv', 'w', newline='') as csvfile:
   # These are the header row values at the top.
   fieldnames = ['first_name', 'last_name','job_title', 'hire_date','performance_review' ]
   # This opens the `DictWriter`. 
   writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
   # Write out the header row (this only needs to be done once!). 
-  writer.writeheader(employees)
+  writer.writeheader()
   # Write as many rows as you want! 
-  # writer.writerow({
-  #   "first_name": "Bill", 
-  #   "last_name": "Lumbergh",
-  #   "job_title": "Vice President",
-  #   "hire_date": 1985,
-  #   "performance_review": "excellent"
-  # }) 
-  # writer.writerow({
-  #   "first_name": "Michael", 
-  #   "last_name": "Bolton",
-  #   "job_title": "Programmer",
-  #   "hire_date": 1995,
-  #   "performance_review": "poor"
-  # }) 
-  # writer.writerow({
-  #   "first_name": "Peter", 
-  #   "last_name": "Gibbons",
-  #   "job_title": "Programmer",
-  #   "hire_date": 1989,
-  #   "performance_review": "poor"
-  # })
+  # for i in emplpoyees:
+  for i in len(employees):
+    writer.writerow(employees[i]) 
+
 
 # Report is a list of objects. # The keys in these objects will be the header fields of your spreadsheet.
 # report = [{...}, {...}, {...}]
